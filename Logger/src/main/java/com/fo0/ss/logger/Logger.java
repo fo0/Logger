dce4fdb81a71b17a21ac7315a6e3842cc217659c
max <max@Coding>
2016-11-18 00:27:50 +0100

    refactors and console info change

--- Logger/src/main/java/com/fo0/ss/logger/Logger.java
+++ Logger/src/main/java/com/fo0/ss/logger/Logger.java
@@ -13,9 +13,9 @@ public class Logger extends LogManager {
 		ConfigureLogger.initialize(os);
 		log.info("#########################################");
 		log.info("Logger-Configure Options:");
-		log.info("\t Logger-Path: " + CONSTANTS.path);
-		log.info("\t Logger-Level: " + CONSTANTS.LOGGER_LEVEL);
-		log.info("\t Detected OS: " + os);
+		log.info(" Logger-Path: " + CONSTANTS.path);
+		log.info(" Logger-Level: " + CONSTANTS.LOGGER_LEVEL);
+		log.info(" Detected OS: " + os);
 		log.info("Logger has been successful initialized");
 		log.info("#########################################");
 		System.out.println();
